203d5fd6817c76fe57ac8b5fe0eb24aaa075baf0
Tim Meneshev <tim130512@ya.ru>
2026-01-16 20:16:15 +0700

    internal commit

--- stats/stats-client/src/main/java/client/StatsClient.java
+++ stats/stats-client/src/main/java/client/StatsClient.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import model.EndpointHitDto;
 import model.ViewStatsDto;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
@@ -15,45 +16,43 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-//TODO
-// 3. Сделать DiscoveryClient и все требуемые настройки в ветке spring-cloud
-// 3. Сделать PR на main
-// настройка роутов
 
 @Component
 @RequiredArgsConstructor
 public class StatsClient {
-    private final RestClient restClient;
+    private final RestClient.Builder restClientBuilder;
     private final DiscoveryClient discoveryClient;
+
+    @Value("${services.stats-service-id:stats-server}")
     private String statsServiceId;
 
+    private String getStatsServiceUrl() {
+        ServiceInstance serviceInstance = discoveryClient.getInstances(statsServiceId).stream()
+                .findFirst()
+                .orElseThrow(() -> new StatsServerUnavailable("Сервис статистики с id: " + statsServiceId
+                        + " не найден в реестре"));
 
-//    public StatsClient(RestClient.Builder restClientBuilder,
-//                       @Value("${stats.server.url:http://localhost:9090}") String serverUrl) {
-//        this.restClient = restClientBuilder
-//                .baseUrl(serverUrl)
-//                .build();
-//    }
+        return String.format("http://%s:%d/stats", serviceInstance.getHost(), serviceInstance.getPort());
+    }
 
-    private StatsClient getInstance() {
-        try {
-            return discoveryClient
-                    .getInstances(statsServiceId)
-                    .getFirst();
-        } catch (Exception e) {
-            throw new StatsServerUnavailable("Ошибка обнаружения адреса сервиса статистики с id: " + statsServiceId,
-                    e);
-        }
+    private RestClient getRestClient() {
+        return restClientBuilder
+                .baseUrl(getStatsServiceUrl())
+                .build();
     }
 
     public void hit(EndpointHitDto endpointHitDto) {
         try {
+            RestClient restClient = getRestClient();
+
             restClient.post()
                     .uri("/hit")
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(endpointHitDto)
                     .retrieve()
                     .toBodilessEntity();
+        } catch (StatsServerUnavailable e) {
+            throw e;
         } catch (Exception e) {
             System.err.println("Ошибка при отправке статистики: " + e.getMessage());
         }
@@ -73,10 +72,14 @@ public class StatsClient {
                 uris.forEach(uri -> uriBuilder.queryParam("uris", uri));
             }
 
+            RestClient restClient = getRestClient();
+
             return restClient.get()
                     .uri(uriBuilder.build().toUriString())
                     .retrieve()
                     .body(new ParameterizedTypeReference<>() {});
+        } catch (StatsServerUnavailable e) {
+            throw e;
         } catch (Exception e) {
             System.err.println("Ошибка при получении статистики: " + e.getMessage());
             return List.of();
--- stats/stats-server/src/main/java/server/StatsServerApplication.java
+++ stats/stats-server/src/main/java/server/StatsServerApplication.java
@@ -2,9 +2,11 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"server", "error"})
 public class StatsServerApplication {
     public static void main(String[] args) {
